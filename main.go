c807388e28dc8f2a183e209aff2fb026ebb5c363
ivan <vannok@live.com>
2024-04-25 23:55:58 +0300

    cors

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/beevik/guid"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/samber/lo"
 	"net/http"
 )
 
@@ -17,11 +19,19 @@ type Patient struct {
 type PatientInput struct {
 	Fullname string `json:"fullname" binding:"required"`
 	Birthday string `json:"birthday" binding:"required"`
-	Gender   int    `json:"gender" binding:"required"`
+	Gender   *int   `json:"gender" binding:"required"`
 }
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		patients, err := patientJSONtoStruct()
@@ -37,6 +47,7 @@ func main() {
 
 	r.POST("/", func(c *gin.Context) {
 		var input PatientInput
+		fmt.Println(input)
 		if err := c.Bind(&input); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
@@ -46,7 +57,7 @@ func main() {
 		patient := Patient{
 			Fullname: input.Fullname,
 			Birthday: input.Birthday,
-			Gender:   input.Gender,
+			Gender:   *input.Gender,
 			Guid:     g,
 		}
 
@@ -78,12 +89,11 @@ func main() {
 			return
 		}
 
-		g := guid.NewString()
 		patient := Patient{
 			Fullname: input.Fullname,
 			Birthday: input.Birthday,
-			Gender:   input.Gender,
-			Guid:     g,
+			Gender:   *input.Gender,
+			Guid:     id,
 		}
 
 		patients, err := patientJSONtoStruct()
@@ -114,13 +124,21 @@ func main() {
 			return
 		}
 
+		_, ok := lo.Find(*patients, func(p Patient) bool {
+			return p.Guid == id
+		})
+		if !ok {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Can't find patient with id: " + id})
+			return
+		}
+
 		err = removeFromJSON(id, *patients)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"patient": id})
+		c.JSON(http.StatusOK, gin.H{"patientID": id})
 	})
 
 	err := r.Run()
